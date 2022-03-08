df377bfa889a00d90f73bb6b4518262b808a2069
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-08 11:06:29 +0700

    connect_db_#21

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,12 @@
-let getHomePage = (req, res) => {
-  return res.render("homepage.ejs");
+import db from "../models/index";
+let getHomePage = async (req, res) => {
+  try {
+    let data = await db.User.findAll();
+    console.log(data);
+    return res.render("homepage.ejs", { data: JSON.stringify(data) });
+  } catch (error) {
+    console.log(error);
+  }
 };
 module.exports = {
   getHomePage: getHomePage,
--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initWebRouters from "./route/web";
+import connectDB from "./config/connectDB";
 require("dotenv").config();
 let app = express(); //
 //config app
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 initWebRouters(app);
+connectDB();
 
 let port = process.env.PORT || 8080;
 
