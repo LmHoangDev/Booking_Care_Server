bb2de8ea301b910a55d31d279ec495e2d6eee5a9
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-09 17:19:02 +0700

    api/login_server

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 let router = express.Router();
 
 let initWebRouters = (app) => {
@@ -10,6 +11,9 @@ let initWebRouters = (app) => {
   router.get("/edit-crud", homeController.getEditCRUD);
   router.post("/put-crud", homeController.putCRUD);
   router.get("/delete-crud", homeController.deleteCRUD);
+
+  //api
+  router.post("/api/login", userController.handleLogin);
   return app.use("/", router);
 };
 
