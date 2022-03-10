d7de4f61844b5aa1cf87009867526c55883ed9ef
Hoang Lam <95642674+LmHoangDev@users.noreply.github.com>
2022-03-10 17:42:30 +0700

    edit/api_get_all_users#38

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -24,7 +24,7 @@ let handleLogin = async (req, res) => {
   });
 };
 let handleGetAllUsers = async (req, res) => {
-  let id = req.body.id;
+  let id = req.query.id;
   console.log(id);
   if (!id) {
     return res.status(200).json({
